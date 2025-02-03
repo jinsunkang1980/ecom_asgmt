1877d2a7858517d5e89eda47c77bd22f82c1832d
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-02-03 08:46:12 -0500

    Update logout.php

--- ecommerce/logout.php
+++ ecommerce/logout.php
@@ -1,9 +1,9 @@
 <?php
-// Clear user cookies by setting them with an expired time
+
 setcookie('user_id', '', time() - 3600, '/');
 setcookie('user_name', '', time() - 3600, '/');
 
-// Redirect to homepage after logout
+
 header("Location: index.php");
 exit();
 ?>
